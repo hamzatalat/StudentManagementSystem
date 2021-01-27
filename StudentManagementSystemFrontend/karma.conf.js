18b517010493b1d93e7c70a37e705b54ea666a75
hamzatalat <hamza_talat@diyatech.com>
2021-01-27 18:23:40 +0500

    Changed Frontend

--- StudentManagementSystemFrontend/karma.conf.js
+++ StudentManagementSystemFrontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/StudentManagementSystemFrontend'),
+      dir: require('path').join(__dirname, './coverage/StudentsManagementSystemFrontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
